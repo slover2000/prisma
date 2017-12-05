2bf4cb894bd133aad5f107626733b075b9a76469
Jiang Lifeng <jianglifeng@xiaomi.com>
2017-12-05 18:14:37 +0800

    update

--- metrics/prometheus/counter.go
+++ metrics/prometheus/counter.go
@@ -17,8 +17,9 @@ const (
 )
 
 type prometheusClient struct {
-	qpsCounter        *prom.CounterVec
-	latencyHistogram  *prom.HistogramVec
+	totalCounter       *prom.CounterVec
+	errorCounter       *prom.CounterVec		
+	durationHistogram  *prom.HistogramVec
 }
 
 // NewGRPCClientPrometheus returns a ClientMetrics object. Use a new instance of
@@ -27,23 +28,30 @@ type prometheusClient struct {
 // opposed to automatically adding metrics via init functions.
 func NewGRPCClientPrometheus(project string) m.ClientMetrics {
 	client := &prometheusClient{
-		qpsCounter: prom.NewCounterVec(
+		totalCounter: prom.NewCounterVec(
 			prom.CounterOpts{
-				Name: fmt.Sprintf("%s_grpc_client_qps", project),
+				Name: fmt.Sprintf("%s_grpc_request_total", project),
 				Help: fmt.Sprintf("%s: total number of RPCs completed by the client, regardless of success or failure.", project),
 			}, []string{"service", "method", "code"}),
+			
+		errorCounter: prom.NewCounterVec(
+			prom.CounterOpts{
+				Name: fmt.Sprintf("%s_grpc_request_failures_total", project),
+				Help: fmt.Sprintf("%s: total number of RPCs failed by the client.", project),
+			}, []string{"service", "method", "code"}),			
 
-		latencyHistogram: prom.NewHistogramVec(
+		durationHistogram: prom.NewHistogramVec(
 			prom.HistogramOpts{
-				Name: fmt.Sprintf("%s_grpc_client_latency", project),
+				Name: fmt.Sprintf("%s_grpc_request_duration_ms", project),
 				Help: fmt.Sprintf("%s: histogram of response latency (milliseconds) of the gRPC until it is finished by the application.", project),
 				Buckets: prom.DefBuckets,
 			},
 			[]string{"service", "method", "code"},
 		),
 	}
-	prom.MustRegister(client.qpsCounter)
-	prom.MustRegister(client.latencyHistogram)
+	prom.MustRegister(client.totalCounter)
+	prom.MustRegister(client.errorCounter)
+	prom.MustRegister(client.durationHistogram)
 
 	return client
 }
@@ -54,23 +62,30 @@ func NewGRPCClientPrometheus(project string) m.ClientMetrics {
 // opposed to automatically adding metrics via init functions.
 func NewGRPCServerPrometheus(project string) m.ClientMetrics {
 	client := &prometheusClient{
-		qpsCounter: prom.NewCounterVec(
+		totalCounter: prom.NewCounterVec(
 			prom.CounterOpts{
-				Name: fmt.Sprintf("%s_grpc_server_qps", project),
+				Name: fmt.Sprintf("%s_grpc_service_total", project),
 				Help: fmt.Sprintf("%s: total number of RPCs completed on the server, regardless of success or failure.", project),
 			}, []string{"service", "method", "code"}),
 
-		latencyHistogram: prom.NewHistogramVec(
+		errorCounter: prom.NewCounterVec(
+			prom.CounterOpts{
+				Name: fmt.Sprintf("%s_grpc_service_failures_total", project),
+				Help: fmt.Sprintf("%s: total number of RPCs failed by the client.", project),
+			}, []string{"service", "method", "code"}),			
+
+		durationHistogram: prom.NewHistogramVec(
 			prom.HistogramOpts{
-				Name: fmt.Sprintf("%s_grpc_server_latency", project),
+				Name: fmt.Sprintf("%s_grpc_service_duration_ms", project),
 				Help: fmt.Sprintf("%s: histogram of response latency (seconds) of gRPC that had been application-level handled by the server.", project),
 				Buckets: prom.DefBuckets,
 			},
 			[]string{"service", "method", "code"},
 		),
 	}
-	prom.MustRegister(client.qpsCounter)
-	prom.MustRegister(client.latencyHistogram)
+	prom.MustRegister(client.totalCounter)
+	prom.MustRegister(client.errorCounter)
+	prom.MustRegister(client.durationHistogram)
 
 	return client
 }
@@ -78,23 +93,30 @@ func NewGRPCServerPrometheus(project string) m.ClientMetrics {
 // NewHTTPClientPrometheus returns a ServerMetrics object.
 func NewHTTPClientPrometheus(project string) m.ClientMetrics {
 	client := &prometheusClient{
-		qpsCounter: prom.NewCounterVec(
+		totalCounter: prom.NewCounterVec(
 			prom.CounterOpts{
-				Name: fmt.Sprintf("%s_http_client_qps", project),
+				Name: fmt.Sprintf("%s_http_request_total", project),
 				Help: fmt.Sprintf("%s: total number of http completed by the client, regardless of success or failure.", project),
 			}, []string{"domain", "path", "method", "code"}),
 
-		latencyHistogram: prom.NewHistogramVec(
+		errorCounter: prom.NewCounterVec(
+			prom.CounterOpts{
+				Name: fmt.Sprintf("%s_http_request_failures_total", project),
+				Help: fmt.Sprintf("%s: total number of http failed by the client.", project),
+			}, []string{"domain", "path", "method", "code"}),
+
+		durationHistogram: prom.NewHistogramVec(
 			prom.HistogramOpts{
-				Name: fmt.Sprintf("%s_http_client_latency", project),
+				Name: fmt.Sprintf("%s_http_request_duration_ms", project),
 				Help: fmt.Sprintf("%s: histogram of response latency (milliseconds) of the http until it is finished by the application.", project),
 				Buckets: prom.DefBuckets,
 			},
 			[]string{ "domain", "path", "method", "code"},
 		),
 	}
-	prom.MustRegister(client.qpsCounter)
-	prom.MustRegister(client.latencyHistogram)
+	prom.MustRegister(client.totalCounter)
+	prom.MustRegister(client.errorCounter)
+	prom.MustRegister(client.durationHistogram)
 
 	return client
 }
@@ -102,23 +124,30 @@ func NewHTTPClientPrometheus(project string) m.ClientMetrics {
 // NewHTTPServerPrometheus returns a ServerMetrics object.
 func NewHTTPServerPrometheus(project string) m.ClientMetrics {
 	client := &prometheusClient{
-		qpsCounter: prom.NewCounterVec(
+		totalCounter: prom.NewCounterVec(
 			prom.CounterOpts{
-				Name: fmt.Sprintf("%s_http_server_qps", project),
+				Name: fmt.Sprintf("%s_http_service_total", project),
 				Help: fmt.Sprintf("%s: total number of http completed by the server, regardless of success or failure.", project),
 			}, []string{"domain", "path", "method", "code"}),
 
-		latencyHistogram: prom.NewHistogramVec(
+		errorCounter: prom.NewCounterVec(
+			prom.CounterOpts{
+				Name: fmt.Sprintf("%s_http_service_failures_total", project),
+				Help: fmt.Sprintf("%s: total number of http failed by the server.", project),
+			}, []string{"domain", "path", "method", "code"}),			
+
+		durationHistogram: prom.NewHistogramVec(
 			prom.HistogramOpts{
-				Name: fmt.Sprintf("%s_http_server_latency", project),
+				Name: fmt.Sprintf("%s_http_service_duration_ms", project),
 				Help: fmt.Sprintf("%s: histogram of response latency (milliseconds) of the http until it is finished by the application.", project),
 				Buckets: prom.DefBuckets,
 			},
 			[]string{"domain", "path", "method", "code"},
 		),
 	}
-	prom.MustRegister(client.qpsCounter)
-	prom.MustRegister(client.latencyHistogram)
+	prom.MustRegister(client.totalCounter)
+	prom.MustRegister(client.errorCounter)
+	prom.MustRegister(client.durationHistogram)
 
 	return client
 }
@@ -130,15 +159,20 @@ func (c *prometheusClient) CounterGRPC(name string, duration time.Duration, err 
 
 	code := grpc.Code(err)
 	serviceName, methodName := m.SplitGRPCMethodName(name)
-	// 记录Counter, like QPS
-	c.qpsCounter.WithLabelValues(serviceName, methodName, code.String()).Inc()
+	// 记录total counter
+	c.totalCounter.WithLabelValues(serviceName, methodName, code.String()).Inc()
+
+	// 记录failurs counter
+	if err != nil {
+		c.errorCounter.WithLabelValues(serviceName, methodName, code.String()).Inc()
+	}
 
 	// 记录Histogram, in millisecond, measure cost time of every method
 	ms := duration.Nanoseconds() / int64(time.Millisecond)
 	if ms == 0 {
 		ms = 1
 	}
-	c.latencyHistogram.WithLabelValues(serviceName, methodName, code.String()).Observe(float64(ms))
+	c.durationHistogram.WithLabelValues(serviceName, methodName, code.String()).Observe(float64(ms))
 }
 
 func (c *prometheusClient) CounterHTTP(req *http.Request, duration time.Duration, code int) {
@@ -146,15 +180,20 @@ func (c *prometheusClient) CounterHTTP(req *http.Request, duration time.Duration
 		return
 	}
 		
-	// 记录Counter, like QPS
-	c.qpsCounter.WithLabelValues(req.URL.Host, req.URL.Path, req.Method, fmt.Sprintf("%d", code)).Inc()
+	// 记录total counter, like QPS
+	c.totalCounter.WithLabelValues(req.URL.Host, req.URL.Path, req.Method, fmt.Sprintf("%d", code)).Inc()
+
+	// 记录failurs counter
+	if code >= 400 {
+		c.errorCounter.WithLabelValues(req.URL.Host, req.URL.Path, req.Method, fmt.Sprintf("%d", code)).Inc()
+	}	
 
 	// 记录Histogram, in millisecond, measure cost time of every method
 	ms := duration.Nanoseconds() / int64(time.Millisecond)
 	if ms == 0 {
 		ms = 1
 	}
-	c.latencyHistogram.WithLabelValues(req.URL.Host, req.URL.Path, req.Method, fmt.Sprintf("%d", code)).Observe(float64(ms))
+	c.durationHistogram.WithLabelValues(req.URL.Host, req.URL.Path, req.Method, fmt.Sprintf("%d", code)).Observe(float64(ms))
 }
 
 func (c *prometheusClient) Close() error {
